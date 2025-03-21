719c5d0355a003f71bbf76cdb6c22fff06648f77
Opango14 <opangotimothy14@gmail.com>
2025-03-21 08:29:01 +0300

    Created the contact page

--- sweatshop/authapp/models.py
+++ sweatshop/authapp/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 # this file we creates database tables
+
+class Contact(models.Model):
+    name=models.CharField(max_length=25)
+    email=models.EmailField()
+    phonenumber=models.CharField(max_length=12)
+    description=models.TextField()
+    def __str__(self):
+        return self.email
