4bf54df1a7fc9fc39ea53b2bd17738ed7404652c
Opango14 <opangotimothy14@gmail.com>
2025-03-21 10:04:37 +0300

    Created tables for enrollment,trainer and mambershipplan

--- sweatshop/authapp/models.py
+++ sweatshop/authapp/models.py
@@ -10,3 +10,35 @@ class Contact(models.Model):
     description=models.TextField()
     def __str__(self):
         return self.email
+    
+class Enrollment(models.Model):
+    Fullname=models.CharField(max_length=25)
+    Email=models.EmailField()
+    Gender=models.CharField(max_length=25)
+    PhoneNumber=models.CharField(max_length=12)
+    DOB=models.DateField(auto_now=False, auto_now_add=False)
+    SelectMembershipPlan=models.CharField(max_length=200)
+    SelectTrainer=models.CharField(max_length=55)
+    Reference=models.CharField(max_length=55)
+    Address=models.TextField()
+    PaymentStatus=models.CharField(max_length=55,blank=True,null=True)
+    Price=models.IntegerField(blank=True,null=True)
+    DueDate=models.DateTimeField(blank=True,null=True)
+    timeStamp=models.DateTimeField(auto_now_add=True,blank=True)
+    def __str__(self):
+        return self.Fullname
+
+class Trainer(models.Model):
+    name=models.CharField(max_length=25)
+    gender=models.CharField(max_length=25)
+    phone=models.CharField(max_length=25)
+    salary=models.IntegerField()
+    timeStamp=models.DateTimeField(auto_now_add=True,blank=True)
+    def __str__(self):
+        return self.name
+
+class MembershipPlan(models.Model):
+    plan=models.CharField(max_length=185)
+    price=models.IntegerField()
+    def __int__(self):
+        return self.id
